b6627ef71995a3f9974fc94a0b02aac4a9377dd7
David Talmage <talmage@acm.org>
2020-10-11 15:14:54 -0400

    Python and JavaScript code reflect all recent changes.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+# Press Button A to decrease the brightness of the HDHalo LEDs.
+
+def on_button_pressed_a():
+    global brightness
+    if brightness > 0:
+        brightness += -1
+        haloDisplay.set_brightness(brightness)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# Press Buttons A and B to display the brightness of the HDHalo LEDs.
+
+def on_button_pressed_ab():
+    basic.show_string("Brightness:")
+    basic.show_number(brightness)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+# Press Button B to increase the brightness of the HDHalo LEDs.
+
+def on_button_pressed_b():
+    global brightness
+    if brightness < 255:
+        brightness += 1
+        haloDisplay.set_brightness(brightness)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+brightness = 0
+haloDisplay: kitronik_halo_hd.ZIPHaloHd = None
 haloDisplay = kitronik_halo_hd.create_zip_halo_display(60)
+brightness = 113
 haloDisplay.clear()
-haloDisplay.set_brightness(113)
+haloDisplay.set_brightness(brightness)
 haloDisplay.show_rainbow(1, 360)
 while True:
     haloDisplay.rotate(1)
--- main.ts
+++ main.ts
@@ -1,24 +1,28 @@
-// Press Button A to decrease the brightness of the HDHalo LEDs.
-input.onButtonPressed(Button.A, function () {
+//  Press Button A to decrease the brightness of the HDHalo LEDs.
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (brightness > 0) {
         brightness += -1
         haloDisplay.setBrightness(brightness)
     }
+    
 })
-// Press Buttons A and B to display the brightness of the HDHalo LEDs.
-input.onButtonPressed(Button.AB, function () {
+//  Press Buttons A and B to display the brightness of the HDHalo LEDs.
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     basic.showString("Brightness:")
     basic.showNumber(brightness)
 })
-// Press Button B to increase the brightness of the HDHalo LEDs.
-input.onButtonPressed(Button.B, function () {
+//  Press Button B to increase the brightness of the HDHalo LEDs.
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (brightness < 255) {
         brightness += 1
         haloDisplay.setBrightness(brightness)
     }
+    
 })
 let brightness = 0
-let haloDisplay: kitronik_halo_hd.ZIPHaloHd = null
+let haloDisplay : kitronik_halo_hd.ZIPHaloHd = null
 haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
 brightness = 113
 haloDisplay.clear()
