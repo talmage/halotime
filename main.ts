09a43b82b59404a2634dc521c9e84166dc944a08
David Talmage <talmage@acm.org>
2020-10-11 11:00:26 -0400

    Press button A to decrease brightness.  Press button B to increase brightness.

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
-let haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
+// Press Button A to decrease the brightness of the LEDs.
+input.onButtonPressed(Button.A, function () {
+    if (brightness > 0) {
+        brightness += -1
+        haloDisplay.setBrightness(brightness)
+    }
+})
+// Press Button B to increase the brightness of the LEDs.
+input.onButtonPressed(Button.B, function () {
+    if (brightness < 255) {
+        brightness += 1
+        haloDisplay.setBrightness(brightness)
+    }
+})
+let brightness = 0
+let haloDisplay: kitronik_halo_hd.ZIPHaloHd = null
+haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
+brightness = 113
 haloDisplay.clear()
-haloDisplay.setBrightness(113)
+haloDisplay.setBrightness(brightness)
 haloDisplay.showRainbow(1, 360)
 while (true) {
     haloDisplay.rotate(1)
